e96cbf71789e30110db9a47cdc297e07524f5d01
Megan Slater <megan.e.slater@gmail.com>
2015-07-24 11:39:05 -0700

    cleanup unecessary code

--- imagersite/imager_images/models.py
+++ imagersite/imager_images/models.py
@@ -7,7 +7,7 @@ from imager_profile.models import ImagerProfile
 class Photo(models.Model):
     file = models.ImageField(upload_to='photo_files/%Y-%m-%d')
     title = models.CharField(max_length=128,
-        help_text="What is your photo's title?")
+                             help_text="What is your photo's title?")
     description = models.TextField(help_text="Describe your photo.")
     date_uploaded = models.DateField(auto_now_add=True)
     date_modified = models.DateField(auto_now=True)
@@ -19,7 +19,7 @@ class Photo(models.Model):
         ('Shared', 'Shared'),
     )
     published = models.CharField(max_length=8, choices=CHOICES,
-        default='Private')
+                                 default='Private')
 
     user = models.ForeignKey(ImagerProfile, related_name='photos', null=False)
 
@@ -30,13 +30,18 @@ class Photo(models.Model):
 @python_2_unicode_compatible
 class Album(models.Model):
     user = models.ForeignKey(ImagerProfile, related_name='albums', null=False)
-    photos = models.ManyToManyField(Photo, related_name='albums')
+    photos = models.ManyToManyField(Photo, related_name='albums',)
     title = models.CharField(max_length=128,
-        help_text="What is your album's title?")
+                             help_text="What is your album's title?")
     description = models.TextField(help_text="Describe your album.")
     date_created = models.DateField(auto_now_add=True)
     date_modified = models.DateField(auto_now=True)
     date_published = models.DateField(auto_now=True)
+
+    def clean(self):
+        for photo in self.photos:
+            if photo.user is not self.user:
+                photo = None
 
     CHOICES = (
         ('Public', 'Public'),
@@ -44,15 +49,9 @@ class Album(models.Model):
         ('Shared', 'Shared'),
     )
     published = models.CharField(max_length=8, choices=CHOICES,
-        default='Private')
+                                 default='Private')
 
     cover = models.ForeignKey(Photo, related_name='cover_for')
 
     def __str__(self):
         return self.title
-
-
-# @python_2_unicode_compatible
-# class Cover(models.Model):
-#     photo = models.ForeignKey(Photo, related_name='covers')
-#     album = models.OneToOneField(Album, related_name='cover')
