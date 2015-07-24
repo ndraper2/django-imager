4a65a0231fd25bb186f1dcb17670c57c9b644bc5
Megan Slater <megan.e.slater@gmail.com>
2015-07-24 14:10:57 -0700

    updated tests so setup and teardown are correct

--- imagersite/imager_images/tests.py
+++ imagersite/imager_images/tests.py
@@ -2,8 +2,9 @@ from __future__ import unicode_literals
 from django.test import TestCase
 import factory
 from django.contrib.auth.models import User
-from imager_profile.models import ImagerProfile
 from faker import Faker as fake_factory
+from models import Photo, Album
+from imager_profile.models import ImagerProfile
 
 fake = fake_factory()
 
@@ -18,39 +19,79 @@ class UserFactory(factory.Factory):
     username = factory.Sequence(lambda n: 'user{}'.format(n))
 
 
-class ProfileTestCase(TestCase):
-    users = []
+class PhotoFactory(factory.Factory):
+    class Meta:
+        model = Photo
+    title = fake.sentences(nb=1)
+    description = fake.text()
+    file = fake.mime_type()
+
+
+class AlbumFactory(factory.Factory):
+    class Meta:
+        model = Album
+    title = fake.sentences(nb=1)
+    description = fake.text()
+
+
+class PhotoTestCase(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        super(PhotoTestCase, cls).setUpClass()
+        user = UserFactory.create()
+        user.save()
+        for i in range(100):
+            _user = ImagerProfile.objects.get(user__username='user0')
+            photo = PhotoFactory.create(user=_user)
+            import pdb; pdb.set_trace()
+            photo.save()
 
     @classmethod
-    def setUpClass(self):
-        for i in range(1000):
-            user = UserFactory.create()
-            user.save()
-            self.users.append(user)
+    def tearDownClass(cls):
+        User.objects.all().delete()
+        super(PhotoTestCase, cls).tearDownClass()
 
-    @classmethod
-    def tearDownClass(self):
-        pass
+    def test_photos_are_created(self):
+        self.assertTrue(Photo.objects.count() == 100)
 
-    def test_profile_is_created_when_user_is_saved(self):
-        self.assertTrue(ImagerProfile.objects.count() == 1000)
+    def test_photos_belong_to_user(self):
+        user = ImagerProfile.objects.get(user__username='user0')
+        self.assertEqual(100, len(user.photos.all()))
 
-    def test_profile_str_is_user_username(self):
-        profile = ImagerProfile.objects.get(user__username='user100')
-        self.assertEqual(str(profile), profile.user.username)
+    def test_photos_do_not_belong_to_other_user(self):
+        new_user = UserFactory.create()
+        new_user.save()
+        with self.assertRaises(AttributeError):
+            new_user.photos.all()
 
-    def test_delete_user(self):
-        profile = User.objects.get(username='user100')
-        profile.delete()
-        with self.assertRaises(ImagerProfile.DoesNotExist):
-            ImagerProfile.objects.get(user__username='user100')
 
-    def test_attributes_save(self):
-        profile = ImagerProfile.objects.get(user__username='user0')
-        profile.camera = 'Most amazing camera'
-        profile.address = '123 amazing drive'
-        profile.website = 'www.amazing.com'
-        profile.photography_type = 'amazing'
-        profile.save()
-        self.assertTrue(ImagerProfile.objects.count() == 1000)
-        self.assertEqual('Most amazing camera', profile.camera)
+# class AlbumTestCase(TestCase):
+#     @classmethod
+#     def setUpClass(cls):
+#         super(AlbumTestCase, cls).setUpClass()
+#         _user = UserFactory.create()
+#         _user.save()
+#         _cover = PhotoFactory.create(user=_user.profile)
+#         _cover.save()
+#         import pdb; pdb.set_trace()
+#         for i in range(5):
+#             album = AlbumFactory.create(cover=_cover, user=_user.profile)
+#             album.save()
+
+#     @classmethod
+#     def tearDownClass(cls):
+#         Album.objects.all().delete()
+#         super(AlbumTestCase, cls).tearDownClass()
+
+#     def test_albums_are_created(self):
+#         self.assertTrue(Album.objects.count() == 5)
+
+    # def test_attributes_save(self):
+    #     profile = ImagerProfile.objects.get(user__username='user0')
+    #     profile.camera = 'Most amazing camera'
+    #     profile.address = '123 amazing drive'
+    #     profile.website = 'www.amazing.com'
+    #     profile.photography_type = 'amazing'
+    #     profile.save()
+    #     self.assertTrue(ImagerProfile.objects.count() == 1000)
+    #     self.assertEqual('Most amazing camera', profile.camera)
--- imagersite/imager_profile/tests.py
+++ imagersite/imager_profile/tests.py
@@ -1,3 +1,56 @@
+from __future__ import unicode_literals
 from django.test import TestCase
+import factory
+from django.contrib.auth.models import User
+from imager_profile.models import ImagerProfile
+from faker import Faker as fake_factory
 
-# Create your tests here.
+fake = fake_factory()
+
+
+class UserFactory(factory.Factory):
+    class Meta:
+        model = User
+    first_name = fake.first_name()
+    last_name = fake.last_name()
+    email = factory.LazyAttribute(lambda x:
+                                  '{0}@example.com'.format(x.first_name))
+    username = factory.Sequence(lambda n: 'user{}'.format(n))
+
+
+class ProfileTestCase(TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        super(ProfileTestCase, cls).setUpClass()
+        for i in range(100):
+            user = UserFactory.create()
+            user.save()
+
+    @classmethod
+    def tearDownClass(cls):
+        User.objects.all().delete()
+        super(ProfileTestCase, cls).tearDownClass()
+
+    def test_profile_is_created_when_user_is_saved(self):
+        self.assertTrue(ImagerProfile.objects.count() == 100)
+
+    def test_profile_str_is_user_username(self):
+        profile = ImagerProfile.objects.get(user__username='user50')
+        self.assertEqual(str(profile), profile.user.username)
+
+    def test_delete_user(self):
+        profile = User.objects.get(username='user99')
+        profile.delete()
+        with self.assertRaises(ImagerProfile.DoesNotExist):
+            ImagerProfile.objects.get(user__username='user99')
+
+    def test_attributes_save(self):
+        profile = ImagerProfile.objects.get(user__username='user0')
+        profile.camera = 'Most amazing camera'
+        profile.address = '123 amazing drive'
+        profile.website = 'www.amazing.com'
+        profile.photography_type = 'amazing'
+        profile.save()
+        self.assertTrue(ImagerProfile.objects.count() == 100)
+        self.assertEqual('Most amazing camera', profile.camera)
