b53332b53e41dc0eb818b03f2caaca36e63d845b
Megan Slater <megan.e.slater@gmail.com>
2015-07-29 19:36:54 -0700

    update with album and photo views

--- imagersite/imager_images/urls.py
+++ imagersite/imager_images/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url
-from imager_images.views import PhotoView
+from imager_images.views import PhotoView, AlbumView
 from django.contrib.auth.decorators import login_required
 from django.views.generic import TemplateView
 
@@ -9,4 +9,6 @@ urlpatterns = [
     url(r'^library/', login_required(TemplateView.as_view(
                                      template_name='library.html')),
         name='library'),
+    url(r'^album/(?P<pk>\d+)/$', login_required(AlbumView.as_view()),
+        name='album_detail')
 ]
