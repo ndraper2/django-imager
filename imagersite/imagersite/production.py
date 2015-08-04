7390040e3e3fa8f231c004d15757bef3d7db7321
Megan Slater <megan.e.slater@gmail.com>
2015-08-04 15:40:28 -0700

    updated hosts

--- imagersite/imagersite/production.py
+++ imagersite/imagersite/production.py
@@ -2,5 +2,6 @@ from settings import *
 
 DEBUG = False
 TEMPLATE_DEBUG = False
-ALLOWED_HOSTS = ['<your instance public dns>', 'localhost']
+ALLOWED_HOSTS = ['ec2-54-186-13-120.us-west-2.compute.amazonaws.com',
+                 'localhost']
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
