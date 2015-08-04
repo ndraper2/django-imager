203032ab5eaf9ff091f44e87dc5ec52c499e5ecf
Megan Slater <megan.e.slater@gmail.com>
2015-08-04 16:13:11 -0700

    trying new static root setting in production

--- imagersite/imagersite/production.py
+++ imagersite/imagersite/production.py
@@ -4,5 +4,3 @@ DEBUG = False
 TEMPLATE_DEBUG = False
 ALLOWED_HOSTS = ['ec2-54-186-13-120.us-west-2.compute.amazonaws.com',
                  'localhost']
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
