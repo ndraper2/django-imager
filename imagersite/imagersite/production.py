444a5c4b0182eb4eb21efac3e14922442cd1130f
Megan Slater <megan.e.slater@gmail.com>
2015-08-04 15:54:06 -0700

    removed base dir

--- imagersite/imagersite/production.py
+++ imagersite/imagersite/production.py
@@ -4,5 +4,5 @@ DEBUG = False
 TEMPLATE_DEBUG = False
 ALLOWED_HOSTS = ['ec2-54-186-13-120.us-west-2.compute.amazonaws.com',
                  'localhost']
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
