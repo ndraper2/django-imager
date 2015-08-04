26419f37e55567ef225bec89c7b395d482c193bd
Megan Slater <megan.e.slater@gmail.com>
2015-08-04 14:58:17 -0700

    updated dj_database_url

--- imagersite/imagersite/settings.py
+++ imagersite/imagersite/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 import sys
+import dj_database_url
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -90,10 +91,7 @@ WSGI_APPLICATION = 'imagersite.wsgi.application'
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=('postgres:///' +
-                 os.path.join(BASE_DIR,
-                 'django.db.backends.postgresql_psycopg2')))
+    'default': dj_database_url.config(default='postgres///')
 }
 
 
