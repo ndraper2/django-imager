929af32b08a6ad71c3dc4e38486fc51d63d20e99
Nicholas Draper <ndraper2@gmail.com>
2015-08-04 17:47:00 -0700

    variables for email

--- imagersite/imagersite/settings.py
+++ imagersite/imagersite/settings.py
@@ -129,4 +129,14 @@ INCLUDE_AUTH_URLS = False
 
 SITE_ID = 1
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', '')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
+DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL', '')
+DEFAULT_TO_EMAIL = ''
+
+if DEBUG:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
