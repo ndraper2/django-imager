523e99a6a35cd37f979ad0ba0569c67911f9d991
Nicholas Draper <ndraper2@gmail.com>
2015-08-04 17:48:48 -0700

    moved email production settings to production

--- imagersite/imagersite/production.py
+++ imagersite/imagersite/production.py
@@ -6,3 +6,12 @@ ALLOWED_HOSTS = ['ec2-54-186-13-120.us-west-2.compute.amazonaws.com',
                  'localhost']
 CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', '')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
+DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL', '')
+DEFAULT_TO_EMAIL = ''
