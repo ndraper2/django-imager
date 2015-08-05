c1a9da5afb9b3bd5900cc9bb1f66e33a036733e8
Megan Slater <megan.e.slater@gmail.com>
2015-08-04 17:43:56 -0700

    added cookie preferences for production

--- imagersite/imagersite/production.py
+++ imagersite/imagersite/production.py
@@ -4,3 +4,5 @@ DEBUG = False
 TEMPLATE_DEBUG = False
 ALLOWED_HOSTS = ['ec2-54-186-13-120.us-west-2.compute.amazonaws.com',
                  'localhost']
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
