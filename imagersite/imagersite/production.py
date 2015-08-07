a56900f65f761d177428cf79f16987ae6be3d878
Megan Slater <megan.e.slater@gmail.com>
2015-08-07 12:15:28 -0700

    updated allowed hosts to environ variable

--- imagersite/imagersite/production.py
+++ imagersite/imagersite/production.py
@@ -2,8 +2,6 @@ from settings import *
 
 DEBUG = False
 TEMPLATE_DEBUG = False
-ALLOWED_HOSTS = ['ec2-52-27-126-204.us-west-2.compute.amazonaws.com',
-                 'localhost']
 CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
 
