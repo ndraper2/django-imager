f596af76db49175f1f9cfbb0a059c8d2765904f5
Megan Slater <megan.e.slater@gmail.com>
2015-08-07 15:38:07 -0700

    set debug to false

--- imagersite/imagersite/production.py
+++ imagersite/imagersite/production.py
@@ -1,7 +1,7 @@
 from settings import *
 
-DEBUG = True
-TEMPLATE_DEBUG = True
+DEBUG = False
+TEMPLATE_DEBUG = False
 ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOSTS', '')]
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
