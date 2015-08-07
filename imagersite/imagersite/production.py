52c20dc02bca46d342c60204f6eae2d787b6c5ca
Megan Slater <megan.e.slater@gmail.com>
2015-08-07 15:54:51 -0700

    trying debug true

--- imagersite/imagersite/production.py
+++ imagersite/imagersite/production.py
@@ -1,6 +1,6 @@
 from settings import *
 
-DEBUG = False
+DEBUG = True
 TEMPLATE_DEBUG = False
 ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOSTS', '')]
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
