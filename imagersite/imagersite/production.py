a24594a3f41702f974d75fef77cd40e8c78bd327
Megan Slater <megan.e.slater@gmail.com>
2015-08-07 12:09:59 -0700

    updaing host with this host

--- imagersite/imagersite/production.py
+++ imagersite/imagersite/production.py
@@ -2,7 +2,7 @@ from settings import *
 
 DEBUG = False
 TEMPLATE_DEBUG = False
-ALLOWED_HOSTS = ['ec2-54-186-13-120.us-west-2.compute.amazonaws.com',
+ALLOWED_HOSTS = ['ec2-52-27-126-204.us-west-2.compute.amazonaws.com',
                  'localhost']
 CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
