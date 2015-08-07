12ddbe4862ecb3bbaf77425b47d2a66733ca3acc
Megan Slater <megan.e.slater@gmail.com>
2015-08-07 12:01:52 -0700

    update debug

--- imagersite/imagersite/production.py
+++ imagersite/imagersite/production.py
@@ -1,7 +1,7 @@
 from settings import *
 
-DEBUG = False
-TEMPLATE_DEBUG = False
+DEBUG = True
+TEMPLATE_DEBUG = True
 ALLOWED_HOSTS = ['ec2-54-186-13-120.us-west-2.compute.amazonaws.com',
                  'localhost']
 CSRF_COOKIE_SECURE = True
